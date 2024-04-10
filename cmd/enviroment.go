f9cbd6ec039c6edb473434346517019e4784b710
paolochmprojects <paolo.dev.projects@gmail.com>
2024-04-10 15:09:04 -0500

    [ADD] new features enviroment and db connection

---
+++ cmd/enviroment.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/joho/godotenv"
+
+func loadEnv() error {
+	err := godotenv.Load()
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"log"
+
+	"github.com/paolochmprojects/Apibasego/storage"
+)
+
 func main() {
+
+	// Init env
+	if err := loadEnv(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Init database connection
+	storage.New()
+
+	// instance an app Fiber
 	app := newServer()
 
 	app.Listen(":3000")
